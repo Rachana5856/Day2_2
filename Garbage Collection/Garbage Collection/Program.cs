94a58f61af96608a4aefac777e6d75bbf8451e5b
Rachana5856 <45552880+Rachana5856@users.noreply.github.com>
2022-07-06 20:21:32 +0530

    Add files via upload

---
+++ Garbage Collection/Garbage Collection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garbage_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //number of generation in garbage collection
+            Console.WriteLine("The number of generations are: " +
+                                           GC.MaxGeneration);
+
+            /*This method returns the generation number of the target object.
+            It requires a single parameter i.e. the target object for which the 
+            generation number is required.*/
+            Program obj = new Program();
+            Console.WriteLine("The generation number of object obj is: "
+                                              + GC.GetGeneration(obj));
+
+            //GC.Collect() method
+            GC.Collect(0);
+            Console.WriteLine("Garbage Collection in Generation 0 is: "
+                                              + GC.CollectionCount(0));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Serialisationandeserialisation/Serialisationandeserialisation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialisationandeserialisation
+{
+    [Serializable]
+    class CustomerDetails
+    {
+        public int customerID { get; set; }
+        public string customerName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step1:
+            CustomerDetails obj = new CustomerDetails();
+            obj.customerID = 1;
+            obj.customerName = "John";
+
+            //Step2
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(@"C:\aa\New.txt", FileMode.Create, FileAccess.Write);
+
+            //Step3 Serialise
+            formatter.Serialize(stream, obj);
+            stream.Close();
+
+            //Step4 Deserialize
+            stream = new FileStream(@"C:\aa\New.txt", FileMode.Open, FileAccess.Read);
+            CustomerDetails objnew = (CustomerDetails)formatter.Deserialize(stream);
+
+            Console.WriteLine("After Deserialization Customer Id: " + objnew.customerID);
+            Console.WriteLine("After Deserialization Customer Name: " + objnew.customerName);
+
+            Console.ReadLine();
+        }
+    }
+}
